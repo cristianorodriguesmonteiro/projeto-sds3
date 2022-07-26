d73d94833bebe48f79dc1e2d4902c6a1965eba87
cristianorodriguesmonteiro <cristianoengsof@gmail.com>
2022-07-26 20:49:43 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/cristianorodriguesmonteiro" target="_blank" rel="noreferrer">Cristiano Rodrigues Monteiro</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Projeto aplicação para vendas: <a href="https://www.linkedin.com/in/cristiano-monteiro-047893221/" target="_blank" rel="noreferrer">@ Fabrica de Sites Alter Beach</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
